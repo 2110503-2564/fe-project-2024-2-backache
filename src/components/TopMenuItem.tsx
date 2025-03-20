b9bb241299abee559a7f6e2dcd8d8022d0e090e6
Nuttamas Udomsantitham <6733064521@student.chula.ac.th>
2025-03-20 17:28:31 +0700

    Edit TopMenuItem.tsx

--- src/components/TopMenuItem.tsx
+++ src/components/TopMenuItem.tsx
@@ -2,7 +2,9 @@ import Link from 'next/link';
 
 export default function TopMenuItem ({title, pageRef} : {title: string, pageRef: string}) {
     return (
-        <Link className='text-center text-red-600 text-lg font-semibold mx-5' href={pageRef}>
+        <Link 
+        className='text-center text-red-600 text-lg font-semibold mx-5 hover:underline' 
+        href={pageRef}>
             {title}
         </Link>
     )
