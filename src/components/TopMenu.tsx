8a0136358d56844f92c1e1d261d8bd56d30ee33f
Supanyada Puttharak <6733260121@student.chula.ac.th>
2025-03-20 16:51:22 +0700

    Edit TopMenu

--- src/components/TopMenu.tsx
+++ src/components/TopMenu.tsx
@@ -1,16 +1,24 @@
 import Image from 'next/image'
 import TopMenuItem from './TopMenuItem'
+import Link from 'next/link'
 
 export default function TopMenu() {
     return (
-        <div className="flex items-center h-[60px] z-30">
-            <Image src={'/img/logo.png'} className="w-auto h-[70%] pl-3"
-                alt='logo' width={0} height={0} sizes='100vh'/>
-            <Image src={'/img/logoText.png'} className="w-auto h-[60%] pl-3"
-                alt='logo text' width={0} height={0} sizes='100vh'/>
+        <div className="flex justify-between items-center h-[60px] z-30">
+            <div className="flex items-center">
+                <Image src={'/img/logo.png'} alt='logo' width={0} height={0} sizes='100vh'
+                    className="w-auto h-auto max-w-[70px] max-h-[70px] pl-2"/>
+                <Image src={'/img/logoText.png'} alt='logo text' width={0} height={0} sizes='100vh'
+                     className="w-auto h-[60%] pl-6"/>
+            </div>
+            <div className="flex items-center">
+                <TopMenuItem title='Home' pageRef='/home'/>
+                <TopMenuItem title='Restaurants' pageRef='/restaurants'/>
+                <TopMenuItem title='Reservations' pageRef='/reservations'/>
+
+                <Image src={'/img/profile.png'} alt='logo text' width={0} height={0} sizes='100vh'
+                    className="w-auto h-auto max-w-[60px] max-h-[60px] object-contain pl-2 pr-2"/>
+            </div>
         </div>
-        /*
-        <TopMenuItem title='Booking' pageRef='/booking'/>
-        */
     )
 }
