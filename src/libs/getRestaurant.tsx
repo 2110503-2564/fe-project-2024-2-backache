bb790a43f9b15866e86a5bae58a3f247f4df3db0
isandwish <natthanicha.jjr@gmail.com>
2025-03-20 18:00:57 +0700

    Edit get one restaurant

--- src/libs/getRestaurant.tsx
+++ src/libs/getRestaurant.tsx
@@ -1,4 +1,4 @@
-export default async function getRestaurants(id:string){
+export default async function getRestaurant(id:string){
     const response = await fetch(process.env.BACKEND_URL+`api/v1/restaurants/${id}`);
 
     if(!response.ok){
