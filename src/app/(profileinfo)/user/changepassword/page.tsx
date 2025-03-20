1caa84f8058e2a4a9b9ab9f9ce191c43a1b44ce5
Supanyada Puttharak <6733260121@student.chula.ac.th>
2025-03-21 03:04:56 +0700

    Edit ChangePassword Page

--- src/app/(profileinfo)/user/changepassword/page.tsx
+++ src/app/(profileinfo)/user/changepassword/page.tsx
@@ -1,8 +1,8 @@
-import ChangePasswordPage from "@/components/ChangePasswordPage"
+import ChangePasswordPage from "@/components/ChangePasswordBox"
 
 export default function Home() {
     return (
-        <main>
+        <main className="bg-myred h-[calc(100vh-60px)] justify-center items-center flex flex-col px-80">
             <ChangePasswordPage/>
         </main>
     )
