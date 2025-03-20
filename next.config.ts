35b5e8a8bc283dcf4640997aa7074be54f11faf7
isandwish <natthanicha.jjr@gmail.com>
2025-03-20 15:38:46 +0700

    Edit next config

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,15 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
-  /* config options here */
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  images : {
+      domains : ['drive.google.com'],
+  },
+  experimental : {
+      serverActions : true
+  },
+  env: {
+    FRONTEND_URL: process.env.FRONTEND_URL,
+    BACKEND_URL: process.env.BACKEND_URL
+  }
 };
 
 export default nextConfig;
